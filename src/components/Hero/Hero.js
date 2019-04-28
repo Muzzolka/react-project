5705497920115857346e39e349e2d5696b752ffa
Muzzolka <m.staniszew@gmail.com>
2019-04-29 01:24:54 +0200

    props

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
-const Hero = () => (
+const Hero = props => (
 
     <header className= {styles.component}>
-    <h2 className= {styles.title}>We could be heroes!</h2>
-    <img className= {styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" />
+    <h2 className={styles.title}>{props.titleText}</h2>
+    <img className= {styles.image} src={props.image} />
     </header>
 
 );
+
+Hero.propTypes = {
+    titleText: PropTypes.node,
+    image: PropTypes.string,
+};
 
 export default Hero;
