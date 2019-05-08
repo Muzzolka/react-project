1a51c2dab592182d032afbce3836b45642f7a8e4
Muzzolka <m.staniszew@gmail.com>
2019-05-08 16:25:09 +0200

    redux working

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from './Card.scss';
+import PropTypes from 'prop-types';
 
 class Card extends React.Component {
 
+  static propTypes = {
+    title: PropTypes.node,
+  }
+
   render() {
+    const { title } = this.props;
     return (
       <section className= {styles.component}>
+        {title}
       </section>
     );
   }
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import styles from './Column.scss';
 import Card from '../Card/Card.js';
-import Creator from '../Creator/Creator.js';
+//import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
 import PropTypes from 'prop-types';
-import {settings} from '../../data/dataStore';
+//import {settings} from '../../data/dataStore';
 
 
 class Column extends React.Component {
-
-  state = {
-    cards: this.props.cards || [],
-  }
 
   static propTypes = {
     title: PropTypes.node,
@@ -19,34 +15,21 @@ class Column extends React.Component {
     icon: PropTypes.string,
   }
 
-  addCard(title){
-    this.setState(state => (
-      {
-        cards: [
-          ...state.cards,
-          {
-            key: state.cards[state.cards.length-1].key+1,
-            title,
-            icon: 'list-alt',
-            cards: [],
-          },
-        ],
-      }
-    ));
-  }
-
   render() {
+    const { title, icon, cards } = this.props;
     return (
       <section className= {styles.component}>
-        <h3 className={styles.title}> {this.props.title} 
+        <h3 className={styles.title}> {title} 
           <span className= {styles.icon}>
-            <Icon name={this.props.icon} />
+            <Icon name={icon} />
           </span>
         </h3>
-        {this.state.cards.map(({key, ...columnProps}) => (
-          <Card key={key} {...columnProps} />
+        {cards.map(cardData => (
+          <Card key={cardData.id} {...cardData} />
         ))}
+        {/*
         <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+        */}
       </section>
     );
   }
